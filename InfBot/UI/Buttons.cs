fb8e7b99ce32f11dbec2ec7799bc8fb7da253be7
VVSaltykov <saltyckov.vitya@yandex.ru>
2021-11-05 20:37:33 +0400

    Add subject's buttons

--- InfBot/UI/Buttons.cs
+++ InfBot/UI/Buttons.cs
@@ -20,5 +20,52 @@ namespace InfBot.UI
             });
             ;
         }
+        public static IReplyMarkup Subjects()
+        {
+            return new InlineKeyboardMarkup(new List<List<InlineKeyboardButton>>
+            {
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Диффуры", callbackData: "Диффуры")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Физ-ра", callbackData: "Физика")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "МСОПР", callbackData: "МСОПР")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Физ-ра", callbackData: "Физ-ра")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "ТОИ", callbackData: "ТОИ")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Социология", callbackData: "Социология")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Философия", callbackData: "Философия")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "Программирование", callbackData: "Программирование")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "ТВИМС", callbackData: "ТВИМС")
+                    },
+                    new List<InlineKeyboardButton>
+                    {
+                        InlineKeyboardButton.WithCallbackData(text: "ИнЯз", callbackData: "ИнЯз")
+                    }
+            });
+            ;
+        }
     }
 }
